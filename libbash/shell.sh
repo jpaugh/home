b619a4d4ef0bf89afa87a4d60edcdceb1e733e35
Jonathan Paugh <jonathan.paugh@tylertech.com>
2023-07-18 13:31:54 -0500

    libbash: Add new modules script and shell
    Script has useful scripting utilities, while shell has useful
    UI/interactive utilities

---
+++ libbash/shell.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+isShellInteractive() {
+    case $- in
+        *i*) true;;
+          *) false;;
+    esac
+}
+
+isShellColorable() {
+    [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null
+}
+
+
+if isShellColorable; then
+    # ANSI Colors (convenience variables)
+    declare -A COLOR
+    COLOR[normal]='\e[0m'
+    COLOR[green]='\e[1;32m'
+    COLOR[blue]='\e[1;34m'
+    COLOR[red]='\e[1;31m'
+    COLOR[magenta]='\e[1;35m'
+    COLOR[muteyellow]='\e[1;33m'
+fi
