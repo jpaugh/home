a0099a87529d21aebe342bc63ff10480fa1e5097
Jonathan Paugh <jonathan.paugh@tylertech.com>
2023-07-18 13:30:17 -0500

    Bug libbash sys/msys: conditionally add the nodejs path

--- libbash/sys/msys.sh
+++ libbash/sys/msys.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # MSys specific settings
 
-#[[ -e "/c/Program Files/nodejs" ]] &&
-addPath "/c/Program Files/nodejs"
+[[ -e "/c/Program Files/nodejs" ]] && addPath "/c/Program Files/nodejs"
