61ec68591fc64e7be3ec0dac6a7738be3ea9224e
Jonathan Paugh <jpaugh@pobox.com>
2023-11-28 09:02:07 -0600

    libbash/core: Track previous imports, and don't import twice

--- libbash/core.sh
+++ libbash/core.sh
@@ -4,7 +4,12 @@ LIBBASHDIR="$HOME/libbash"
 
 import () {
     for f in "$@"; do
+        local fname="$(path_to_filename "$f")"
+        import="$__import_${fname}"
+        [[ $import == true ]] && continue
+
         if [[ -f "$LIBBASHDIR/$f.sh" ]]; then
+            declare "__import_${fname}=true"
             source "$LIBBASHDIR/$f.sh"
         else
             echo >&2 "import: not found: '$f.sh'"
