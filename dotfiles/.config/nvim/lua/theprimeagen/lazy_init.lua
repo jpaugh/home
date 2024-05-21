bbc2f6ebaa3cb4f47b93a466e85a6ff667a0ede6
Jonathan Paugh <jpaugh@pobox.com>
2024-05-21 02:37:24 -0500

    Update nvim
    This is what it took to install my neovim config on a new system. I have
    no idea why it went so wrong. I had to manually install
    mason-lspconfig.nvim, which is why it disappeared from the lock file.
    For some reason, it was asking for my github password. That might mean
    that I had misspelled it, or that the repo was renamed. Idk, but it
    works now

--- dotfiles/.config/nvim/lua/theprimeagen/lazy_init.lua
+++ dotfiles/.config/nvim/lua/theprimeagen/lazy_init.lua
@@ -1,11 +1,9 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local dataDir = vim.fn.stdpath("data")
+local lazypath = dataDir .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "branch=stable",
-    lazypath,
-  })
+  print("Downloading lazy to ", dataDir)
+  vim.cmd("!mkdir -p " .. dataDir)
+  vim.cmd("!git clone --filter=blob:none https://github.com/folke/lazy.nvim.git --branch=stable " ..lazypath)
 end
 vim.opt.rtp:prepend(lazypath)
 
